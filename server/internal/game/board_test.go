2521cb083261343624710415b4cc7a4b1836aecf
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-12-18 10:41:04 +0100

    :rotating_light: :white_check_mark: :heavy_check_mark: Added test for board's GenerateEmptyBoard function

---
+++ server/internal/game/board_test.go
@@ -0,0 +1,29 @@
+package game
+
+import (
+	"testing"
+)
+
+func TestGenerateEmptyBoard(t *testing.T) {
+	t.Run("should intialize board with default parameters", func(t *testing.T) {
+		boardSize := 5
+		board_test := GenerateEmptyBoard(boardSize)
+
+		if board_test.Size != boardSize {
+			t.Errorf("board size is expected to be %v got %v", boardSize, board_test.Size)
+		}
+		if len(board_test.Board) != boardSize {
+			t.Errorf("board length is expected to be %v got %v", boardSize, board_test.Size)
+		}
+		for idxi, i := range board_test.Board {
+			for idxj, j := range i {
+				if j.State != Empty {
+					t.Errorf("cell (%v, %v) is expected to be %s", idxi, idxj, Empty)
+				}
+				if j.Position.X != idxi || j.Position.Y != idxj {
+					t.Errorf("Cell Position: want (%v, %v), got (%v, %v)", idxi, idxj, j.Position.X, j.Position.Y)
+				}
+			}
+		}
+	})
+}
