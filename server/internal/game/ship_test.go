a6bcc014d48a49dc45f405ef974b842c5b1fde46
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-12-18 10:40:05 +0100

    :rotating_light: :white_check_mark: :heavy_check_mark: Added test for ship's intialize ship function

---
+++ server/internal/game/ship_test.go
@@ -0,0 +1,24 @@
+package game
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIntializeShips(t *testing.T) {
+	t.Run("should intialize ships with correct values", func(t *testing.T) {
+		want := []Ship{
+			{Type: Carrier, Size: 5, Hits: 0, Sunk: false},
+			{Type: Battleship, Size: 4, Hits: 0, Sunk: false},
+			{Type: Cruiser, Size: 3, Hits: 0, Sunk: false},
+			{Type: Submarine, Size: 3, Hits: 0, Sunk: false},
+			{Type: Destroyer, Size: 2, Hits: 0, Sunk: false},
+		}
+
+		got := InitializeShips()
+
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("wanted %v got %v", want, nil)
+		}
+	})
+}
