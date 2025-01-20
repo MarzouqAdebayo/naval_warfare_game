91e4eb4ff2d926061097d14c6d0d6c03b719d177
DYung26 <theadstore1407@gmail.com>
2025-01-20 10:53:23 +0100

    Implement `user` creation and update functionality

---
+++ server/internal/db/user.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
+
+func (db *Database) SaveUser(username string) (uuid.UUID, error) {
+	var userID uuid.UUID
+	query := `
+		INSERT INTO users (username)
+		VALUES ($1)
+		ON CONFLICT (username)
+		DO UPDATE SET updated_at = CURRENT_TIMESTAMP
+		RETURNING id;
+	`
+	err := db.Conn.Raw(query, username).Scan(&userID).Error
+	if err != nil {
+		return 0, fmt.Errorf("failed to save user: %w", err)
+	}
+	return userID, nil
+}
